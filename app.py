41b09af010031d678dc9455f691da9d18b5b7449
andy <andy@northwalesit.co.uk>
2024-08-09 17:47:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__, template_folder='/home/mungbean90/cloud-interview-task/templates')
+app = Flask(__name__, template_folder='/home/mungbean90/cloud-interview-task/templates/')
 
 
 @app.route('/')
