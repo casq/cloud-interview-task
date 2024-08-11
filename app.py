d1cdcc062d506186a237baeb50df86daaa3163b7
andy <andy@northwalesit.co.uk>
2024-08-11 20:14:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 @app.route('/')
 def home():
-  return render_template('index.htm')
+  return render_template('index.html')
 
 if __name__ == "__main__":
   port = int(os.environ.get('PORT', 3000))
